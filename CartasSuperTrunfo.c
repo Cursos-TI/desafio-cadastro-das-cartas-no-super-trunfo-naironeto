98a5812402b5a0bb4233073d3c5e2d30bffa0db3
naironeto <naironeto@gmail.com>
2025-09-01 23:48:39 +0000

    Adições do nivel mestre. Comparei as cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,6 +15,7 @@ int main() {
     int pontosTuristicos1;
     float densidade1; //Adições do nível aventureiro
     float pib_per_capita1;
+    float super_poder1; // Adição do nível mestre
 
     // Variáveis da segunda carta
     // (copiei as variaveis da primeira carta e substitui o 1 pelo 2, pois o processo é o mesmo.) 
@@ -27,6 +28,7 @@ int main() {
     int pontosTuristicos2;
     float densidade2;
     float pib_per_capita2;
+    float super_poder2; // Adição do nível mestre
 
     
     // Coleta de dados e cadastro das Cartas:
@@ -80,12 +82,15 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
-    // Cálculos das variaveis derivadas
+    // Cálculos das variaveis derivadas (e do super poder no nível mestre).
     densidade1 = populacao1 / area1;
     pib_per_capita1 = pib1 / populacao1;
     densidade2 = populacao2 / area2;
     pib_per_capita2 = pib2 / populacao2;
-
+    super_poder1 = populacao1 + pib1 + area1 + pib_per_capita1 + pontosTuristicos1 + (1/densidade1);
+    super_poder2 = populacao2 + pib2 + area2 + pib_per_capita2 + pontosTuristicos2 + (1/densidade2);
+    
+    
     // Exibição dos Dados das Cartas:
 
     printf("\n--- Dados da primeira carta ---\n");
@@ -110,6 +115,27 @@ int main() {
     printf("Densidade populacional: %.2f habitantes/km²\n", densidade2);
     printf("PIB per capita: %.2f\n", pib_per_capita2);
 
+    // Comparação das cartas
+    /* Nas minhas pesquisas por fora, acabei vendo que eu posso
+    comparar os dados usando o operador ternário, então optei por 
+    essa ferramenta. */
+    int resultado_populacao = populacao1 > populacao2 ? 1 : 0;
+    float resultado_area = area1 > area2 ? 1 : 0; 
+    float resultado_pib = pib1 > pib2 ? 1 : 0;
+    int resultado_pontosTuristicos = pontosTuristicos1 > pontosTuristicos2 ? 1 : 0;
+    float resultado_densidade = densidade1 > densidade2 ? 1 : 0;
+    float resultado_pib_per_capita = pib_per_capita1 > pib_per_capita2 ? 1 : 0;
+    float resultado_super_poder = super_poder1 > super_poder2 ? 1 : 0; 
+
+    printf("\n--- Comparação das cartas ---\n");
+    printf("População A é maior do que B? %d\n", resultado_populacao);
+    printf("Área A é maior do que B? %f\n", resultado_area);
+    printf("PIB A é maior do que B? %f\n", resultado_pib);
+    printf("Tem mais pontos turísticos em A do que em B? %d\n", resultado_pontosTuristicos);
+    printf("Há mais densidade populacional em A do que em B? %f\n", resultado_densidade);
+    printf("O PIB per capita em A é maior que em B? %f\n", resultado_pib_per_capita);
+    printf("Super poder: A é maior que B? %f\n", resultado_super_poder);
+    
 
     return 0;
 }
